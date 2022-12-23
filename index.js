7c7529ae0201ca3657cdc0f65f0f1180b0c9999b
emma-junior <emnzboy31@gmail.com>
2022-12-24 00:04:02 +0100

    cors issue commit

--- index.js
+++ index.js
@@ -18,6 +18,20 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use("/api/products", productsRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
