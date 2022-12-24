aa23e4da70a3e84788005da4be582363967a701d
emma-junior <emnzboy31@gmail.com>
2022-12-24 01:15:59 +0100

    cors issue commit

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // };
 
 app.use(cors());
-
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
